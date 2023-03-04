f7382bf6d2b716d805c66d88cb7c49c9c51cac09
ENIAC_ming <2241967346@qq.com>
2023-03-04 10:57:16 +0800

    -

--- sign.php
+++ sign.php
@@ -3,7 +3,7 @@ if(!session_id())session_start();
 header('Content-Type:application/json; charset=utf-8');
 $pw = $_GET['password'];
 $veri = 'password'; // Set Password Here
-$veri = md5($veri + floor(time()/10000));
+$veri = md5($veri . (string)floor(time()/100));
 if ($pw == $veri)
 {
     $_SESSION['sign'] = true;
